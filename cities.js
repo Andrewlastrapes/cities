648d4f8db709d375c8dea66fcb05d88ca4ee3c4c
Andrew Lastrapes <Andrewlastrapes@gmail.com>
2017-07-18 12:01:23 -0400

    Using filter function

---
+++ cities.js
@@ -0,0 +1,18 @@
+
+var cities = [
+  { name: 'Los Angeles', temperature: 60.0},
+  { name: 'Atlanta', temperature: 52.0 },
+  { name: 'Detroit', temperature: 48.0 },
+  { name: 'New York', temperature: 80.0 }
+];
+
+
+function under70(city){
+	return city.temperature < 70
+	
+}
+
+
+function coldCities(city){
+	return city.filter(under70);
+}
